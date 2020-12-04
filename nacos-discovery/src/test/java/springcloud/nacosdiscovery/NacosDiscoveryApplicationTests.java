565642cb69686fd822b3e2a359f948dbaf8c82de
songjiangyong <songjiangyong@cestc.cn>
2020-12-04 11:01:48 +0800

    注册中心eureka

---
+++ nacos-discovery/src/test/java/springcloud/nacosdiscovery/NacosDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloud.nacosdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
